71809dc876a3777284fa0faee4a90175f8c7810d
Jack York <jack0york@gmail.com>
2021-04-19 12:09:37 -0400

    refactoring

--- For_Fun/lab3/Test.java
+++ For_Fun/lab3/Test.java
@@ -1,14 +1,33 @@
 package For_Fun.lab3;
 
+
 public class Test {
-    public static void main(String[]args){
-        Stack<Object> s = new Stack<>(1,3);
-        s.push("hi");
-        System.out.println(s.pop());
-        System.out.println(s);
-        Queue<Integer> q = new Queue<>(1, 3);
-        q.enqueue(5);
-        System.out.println(q.dequeue());
-        System.out.println(q);
+    public static void main(String[] args) {
+        // Scanner s = new Scanner(System.in);
+        // System.out.println("Enter the possible palindrome: ");
+        // String possible_palindrome = s.nextLine();
+        // System.out.println("\"" + possible_palindrome + "\" is " + (palindrome(possible_palindrome) ? "" : "not ")
+        //         + "a palindrome");
+        // s.close();
+        boolean b1 = false;
+    //  if (condition) expression; else expression;
+        if (b1) System.out.println("true"); else System.out.println("false");
+    //    
+    }
+
+    public static boolean palindrome(String str) {
+        str = str.toUpperCase();
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            while (str.charAt(i) < 'A' || str.charAt(i) > 'Z')
+                i++;
+            while (str.charAt(j) < 'A' || str.charAt(j) > 'Z')
+                j--;
+            if (i < j && str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
     }
 }
