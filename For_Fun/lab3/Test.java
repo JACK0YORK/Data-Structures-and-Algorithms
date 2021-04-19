4554c14b6f4997221af74776523e2a55cade9a51
Jack York <jack0york@gmail.com>
2021-04-19 12:03:22 -0400

    Ignoring my Fun directory

--- For_Fun/lab3/Test.java
+++ For_Fun/lab3/Test.java
@@ -1,7 +1,14 @@
 package For_Fun.lab3;
 
 public class Test {
-    public static void main(String[] args) {
-        System.out.println("To be, ...");
+    public static void main(String[]args){
+        Stack<Object> s = new Stack<>(1,3);
+        s.push("hi");
+        System.out.println(s.pop());
+        System.out.println(s);
+        Queue<Integer> q = new Queue<>(1, 3);
+        q.enqueue(5);
+        System.out.println(q.dequeue());
+        System.out.println(q);
     }
 }
